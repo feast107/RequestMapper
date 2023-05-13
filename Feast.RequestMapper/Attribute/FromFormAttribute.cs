748751322118dab21669d7d58fa194d6090dc1e7
Feast <feast107@gmail.com>
2023-05-13 17:59:00 +0800

    Delete FromFormAttribute.cs

--- Feast.RequestMapper/Attribute/FromFormAttribute.cs
+++
@@ -1,6 +0,0 @@
-﻿using System;
-
-namespace Feast.RequestMapper.Attribute;
-
-[AttributeUsage(AttributeTargets.Property | AttributeTargets.Class)]
-public class FromFormAttribute : System.Attribute { }
