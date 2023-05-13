1d61c4241b49e391b7b54221c40d4706ee0791f3
Feast <feast107@gmail.com>
2023-05-13 18:17:13 +0800

    Create ISerializer.cs

---
+++ Feast.RequestMapper/Interfaces/ISerializer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Feast.RequestMapper.Interfaces
+{
+    public interface ISerializer
+    {
+        object? Deserialize(string input, Type returnType);
+    }
+}
